d314b1d7cee21cbec2af295cf72a7762dd48eb08
Darshkumar Jasani <jasanidarsh3@gmail.com>
2024-01-19 09:34:39 +0530

    File Updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import './App.css'
 import SearchIcon from '@mui/icons-material/Search';
+import MyLocationIcon from '@mui/icons-material/MyLocation';
 const App = ()=>{
   useEffect(()=>{
   })
@@ -8,20 +9,37 @@ const App = ()=>{
     <>
       <div className='buttons'>
         
-        <div className='search'>
-          <SearchIcon/>
-          <input type='text'></input>
-          <button>Search</button>
+        <div className='current'>
+            <button>
+              <div className='div1'> <MyLocationIcon/></div>
+              <div className='div2'> Current Location</div>
+            </button>
         </div>
 
-        <div className='current'></div>
+        <div className='search'>
+          <input type='text' placeholder='Search Location'></input>
+          <SearchIcon/>
+        </div>
+
+        
+
       </div>
 
       <div className='title'>Weather App</div>
 
       <div className='container'>
           <div className='card'>
+              <div className='weatherIcon'>
+                      <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" />
+              </div>
 
+              <div className='temp'>
+                16C
+              </div>
+
+              <div className='loc'>
+                Long Beach
+              </div>
           </div>
       </div>
     </>
