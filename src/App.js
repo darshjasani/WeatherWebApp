4ef05f9e8f00db4da2acf7cf6f802eb6b81c58c1
Darshkumar Jasani <jasanidarsh3@gmail.com>
2024-01-19 11:30:58 +0530

    File Updated

--- src/App.js
+++ src/App.js
@@ -1,40 +1,98 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import SearchIcon from '@mui/icons-material/Search';
 import MyLocationIcon from '@mui/icons-material/MyLocation';
+import axios from 'axios';
 const App = ()=>{
+  const [city, setCity] = useState('');
+  const [current,setCurrent] = useState(0);
+  const [position, setPosition] = useState({latitude : null, longitude : null});
+  const [data, setData] = useState(null);
+  const YOUR_API_KEY = '138c891aa2de594e179138288248aa9b'
+  const fecthData = async() =>{
+    try{
+      let response = '';
+      console.log(current);
+      if(current == 1){
+        response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?appid=${YOUR_API_KEY}&q=${city}&units=metric`
+        );
+      }
+      else{
+        response = await axios.get(
+          `https://api.openweathermap.org/data/2.5/weather?appid=${YOUR_API_KEY}&lat=${position.latitude}&lon=${position.longitude}&units=metric`
+        )
+      }
+      setData(response.data);
+      console.log(response.data);
+    }catch(error){
+      console.error(error);
+    }
+  }
+
   useEffect(()=>{
-  })
+    //getCurrentLocation();
+    fecthData();
+  },[]);
+
+  const handleInputChange = (e) => {
+    setCurrent(1);
+    console.log(e);
+    setCity(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fecthData();
+  }
+
+  const getCurrentLocation = async() =>{
+    
+    if( "geolocation" in navigator){
+      navigator.geolocation.getCurrentPosition((pos)=>{
+        setPosition({
+          latitude : pos.coords.latitude,
+          longitude : pos.coords.longitude
+        });
+      });
+
+    }else{
+      console.log("Geolcation not available in your browser.");
+    }
+    console.log(position);
+    setCurrent(0);
+  }
   return (
     <>
-      <div className='buttons'>
-        
-        <div className='current'>
-            <button>
-              <div className='div1'> <MyLocationIcon/></div>
-              <div className='div2'> Current Location</div>
-            </button>
+      <form onSubmit={handleSubmit}>
+        <div className='buttons'>
+          
+          <div className='current'>
+              <button onClick={getCurrentLocation}>
+                <div className='div1'> <MyLocationIcon/></div>
+                <div className='div2'> Current Location</div>
+              </button>
+          </div>
+
+          <div className='search'>
+            <input type='text' placeholder='Search Location' onChange={handleInputChange}></input>
+            <button type='submit'><SearchIcon/></button>
+          </div> 
+
         </div>
-
-        <div className='search'>
-          <input type='text' placeholder='Search Location'></input>
-          <SearchIcon/>
-        </div>
-
-        
-
-      </div>
+      </form>
 
       <div className='title'>Weather App</div>
 
-      <div className='container'>
+      {data ? (
+        <div className='container'>
           <div className='card'>
               <div className='weatherIcon'>
                       <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" />
               </div>
 
               <div className='temp'>
-                16C
+                {data ? (<div>{data.name} {data.main.temp}</div>) : ('16C') }
               </div>
 
               <div className='loc'>
@@ -42,6 +100,9 @@ const App = ()=>{
               </div>
           </div>
       </div>
+      ) : (
+        <div> Loading Weather Application</div>
+      )}
     </>
   )
 }
